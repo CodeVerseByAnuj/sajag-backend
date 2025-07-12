912835c6da8429a99e0cea5808ff8a0190268f93
CodeVerseByAnuj <anuj.mmmut@gmail.com>
2025-07-12 16:04:37 +0530

    added token

--- src/routes/auth.routes.ts
+++ src/routes/auth.routes.ts
@@ -1,6 +1,7 @@
 import { Router } from "express";
 import { AuthController } from "../controllers/auth.controller";
 import { authLimiter } from "../middlewares/rateLimiter.middleware";
+import { authenticateToken } from "../middlewares/auth.middleware";
 
 const router = Router();
 const authController = new AuthController();
@@ -11,7 +12,7 @@ router.use(authLimiter);
 router.post("/register", authController.register);
 router.post("/login", authController.login);
 router.get("/verify-email", authController.verifyEmail);
-router.get("/refresh-token", authController.refreshToken);
+router.get("/refresh-token",authenticateToken , authController.refreshToken);
 router.post("/logout", authController.logout);
 router.post("/resend-verification", authController.resendVerification);
 
